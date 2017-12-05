295510673f5583383af2e3b11ce7f707644ec5b1
Jose Luis Pintado Vasquez <i201716389@cibertec.edu.pe>
2017-12-05 03:13:06 -0500

    vali

--- Clinica/src/gui/Principal.java
+++ Clinica/src/gui/Principal.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import AppPackage.AnimationClass;
 
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
